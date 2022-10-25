0d4d1e9780c56fff424391249dd80bb3149f14e1
lean <leanjutate@gmail.com>
2022-10-25 19:21:25 -0300

    modo desconectado

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -53,6 +53,7 @@ namespace WinFormsApp1
         {
             var action = new PersonActions();
             mydatset = action.GetPersons();
+            mydatset.Tables[0].TableName = "personas";
             dataGridView1.DataSource = null;
             dataGridView1.DataSource = mydatset.Tables[0];
         }
@@ -60,7 +61,11 @@ namespace WinFormsApp1
         private void button6_Click(object sender, EventArgs e)
         {
             var action = new PersonActions();
-            bool res = action.DeletePersonD(int.Parse(txtdnidel.Text));
+            foreach (DataRow row in mydatset.Tables[0].Select($"dni = {txtdnidel.Text}"))
+            {
+                row.Delete();
+            }
+            bool res = action.DeletePersonD(mydatset);
             MessageBox.Show(res ? "se elimino correctamente" : "no se logro eliminar");
         }
 
@@ -68,15 +73,24 @@ namespace WinFormsApp1
         {
             var action = new PersonActions();
             Person req = new(txtName.Text, txtLastName.Text, int.Parse(txtDni.Text), int.Parse(txtPhone.Text));
-            bool output = action.AddPersonD(req);
-            MessageBox.Show(output ? $"Se inserto correctamente a {req.Name} {req.LastName}" : "No se pudo insertar");
+            mydatset.Tables[0].Rows.Add(null,req.Name, req.LastName,req.Dni,req.Phone);
+            var output = action.AddPersonD(mydatset);
+            MessageBox.Show(output ? $"Se inserto correctamente a {req.Name} {req.LastName}" : "No se pudo insertar en la db");
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
             var action = new PersonActions();
             UpdateDto req = new(txtnew.Text, int.Parse(txtupdni.Text), comboBox1.Text);
-            bool res = action.UpdatePersonD(req);
+            int i = 0;
+            foreach (DataRow row in mydatset.Tables[0].Rows) {
+                if (Convert.ToInt32(row["dni"]) == req.Dni)
+                {
+                    mydatset.Tables[0].Rows[i][req.Type] = req.Newdata;
+                }
+                i++;
+            }
+            bool res = action.UpdatePersonD(mydatset);
             MessageBox.Show(res ? "se actualizo correctamente" : "no se logro actualizar");
         }
     }
